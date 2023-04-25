52a558d97cd41dae81a32afff10450c6aa18a6f1
Saidgp <said.gar.p@gmail.com>
2023-04-25 17:25:02 -0600

    create a new file

--- j03/js03.js
+++ j03/js03.js
@@ -1,1 +1,29 @@
 console.log("Sesion 03");
+
+// ----- Declaración de bloque --------------------
+
+{
+
+    let edadSergio = 39, edadLuis = 25;
+
+    /*
+    Las variables declaradas con let y const
+    solo tendrán alcance(scope) dentro del bloque
+    o bloques anidados.
+
+    */
+   {
+    /*
+     bloque anidado
+     Si una variable local tiene el mismo nombre de
+     una varible global o de bloque superior, la varible
+     local oculta las otras variables, para que la variable
+     local sea usada.
+     */
+    
+   }
+
+   console.log(`Sergio: ${edadSergio} años`);
+    console.log(`Luis ${edadLuis} años`)
+
+}
